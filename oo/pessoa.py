5c9fb0ed6ab2f4c69a68f6e4ac83d14a50a5cffe
josemar <josemar.mascarenhas@unitelco.com.br>
2019-11-13 15:17:48 -0300

    Criados atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None):
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Ola {id(self)}'
 
 if __name__ == '__main__':
-        p = Pessoa()
+        p = Pessoa('Renato')
         print(Pessoa.cumprimentar(p))
         print(id(p))
         print(p.cumprimentar())
+        print(p.nome)
+        p.nome = 'Josemar'
+        print(p.nome)
 
